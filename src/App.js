7d42351cf30150b3c8c23198a455ce16e70ed605
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-24 19:59:47 +0200

    configure home page with router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Root from './components/Root';
+import ErrorPage from './components/ErrorPage';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <ErrorPage />,
+  }
+]);
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 

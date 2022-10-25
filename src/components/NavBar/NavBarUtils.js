e0e748dbf83c58fd10a0dc0ce8af67ae1cd41073
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-25 13:22:13 +0200

    add keys to links array

--- src/components/NavBar/NavBarUtils.js
+++ src/components/NavBar/NavBarUtils.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 export const NavLinks = [
-    <Link to="/" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Home</Link>,
-    <Link to="login" className="list-group-item list-group-item-action bg-info text-white rounded-0">Login</Link>,
-    <Link to="profile" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Profile</Link>
+    <Link key="home" to="/" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Home</Link>,
+    <Link key="login" to="login" className="list-group-item list-group-item-action bg-info text-white rounded-0">Login</Link>,
+    <Link key="profile" to="profile" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Profile</Link>
 ]

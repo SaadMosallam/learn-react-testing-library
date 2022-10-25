42e4b7085a01fe7f6a0995bc70870870877956fb
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-25 18:43:53 +0200

    add Profile tests

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,25 +13,25 @@ export default class Profile extends Component {
     return (
       <>
         <div className='d-flex justify-content-center mt-5'>
-        <div className='card' style={{ width: '18rem' }}>
-          <img
-            className='card-img-top'
-            src={fakeProfile}
-            alt='Card cap'
-          />
-          <div className='card-body'>
-            <h5 className='card-title'>{this.props.name}</h5>
-            <p className='card-subtitle mb-2 text-muted'>{this.props.title}</p>
-            <button onClick={this.setDetails} className='btn btn-primary'>
-              {this.state.showDetails ? 'Hide Details' : 'Display Details'}
-            </button>
+          <div className='card' style={{ width: '18rem' }}>
+            <img
+              className='card-img-top'
+              src={fakeProfile}
+              alt='Profile'
+            />
+            <div className='card-body'>
+              <h5 className='card-title'>{this.props.name}</h5>
+              <p className='card-subtitle mb-2 text-muted'>{this.props.title}</p>
+              <button onClick={this.setDetails} className='btn btn-primary' style={{ width: 144 }}>
+                {this.state.showDetails ? 'Hide Details' : 'Display Details'}
+              </button>
 
-            {this.state.showDetails ? (
-              <p className='card-text details'>{this.props.details}</p>
-            ) : null}
+              {this.state.showDetails ? (
+                <p className='card-text details mt-2'>{this.props.details}</p>
+              ) : null}
+            </div>
           </div>
         </div>
-      </div>
       </>
     );
   }
---
+++ src/components/Profile/Profile.test.js
@@ -0,0 +1,50 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import React from 'react';
+import Profile from './Profile';
+
+describe('Profile tests', () => {
+
+    test('Profile page renders correctly', () => {
+        const userTestData = {
+            name: "John Doe",
+            title: "Software Engineer",
+            details: "I love developing Apps using React"
+        }
+        render(<Profile {...userTestData} />);
+
+        const profilePicture = screen.getByRole('img', { name: /Profile/i });
+        const name = screen.getByRole('heading', { name: userTestData.name });
+        const title = screen.getByText(userTestData.title);
+        const details = screen.getByText(/i love developing apps using react/i);
+        const detailsButton = screen.getByRole('button', { name: /hide details/i });
+
+        expect(profilePicture).toBeInTheDocument();
+        expect(name).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+        expect(details).toBeInTheDocument();
+        expect(detailsButton).toBeInTheDocument();
+    });
+
+    test('Profile Hide details button test', async () => {
+        const user = userEvent.setup();
+        const userTestData = {
+            name: "John Doe",
+            title: "Software Engineer",
+            details: "I love developing Apps using React"
+        }
+        render(<Profile {...userTestData} />);
+
+        const details = screen.getByText(/i love developing apps using react/i);
+        const hideDetailsButton = screen.getByRole('button', { name: /hide details/i });
+
+        expect(hideDetailsButton).toBeInTheDocument();
+        expect(details).toBeInTheDocument();
+
+        await user.click(hideDetailsButton);
+
+        const displayDetailsButton = screen.getByRole('button', { name: /display details/i });
+        expect(displayDetailsButton).toBeInTheDocument();
+        expect(details).not.toBeInTheDocument();
+    });
+});

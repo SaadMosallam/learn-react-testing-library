7a3b7073eaa481a60bd6e050a9d8c60e0526534a
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-25 12:46:10 +0200

    format file

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
 
   return (
     <div className="d-flex align-items-center pt-5 mt-5 flex-column" id="error-page">
-        <img className="mb-5" src={bug} alt="bug" width={300} height={300} />
+      <img className="mb-5" src={bug} alt="bug" width={300} height={300} />
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>

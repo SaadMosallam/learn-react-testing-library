aaf47b6257696c3700954996d1c45e17541b7402
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-25 12:45:29 +0200

    add home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className='d-flex flex-column mt-5 align-items-center'>
+                <h1 className=''>Learn React Testing Library!</h1>
+                <p>Learn how to test React components using React Testing Library.</p>
+            </div>
+        </>
+
+    );
+}
+
+export default Home;

171e2ae24c4fe542694244f458d2828b78b722a2
Smosallam <Saad.MOSALLAM@cegedim.com>
2022-10-31 18:28:37 +0200

    add active NavLink feature

--- src/components/NavBar/NavBarUtils.js
+++ src/components/NavBar/NavBarUtils.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const NavLinks = [
-    <Link key="home" to="/" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Home</Link>,
-    <Link key="login" to="login" className="list-group-item list-group-item-action bg-info text-white rounded-0">Login</Link>,
-    <Link key="profile" to="profile" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Profile</Link>,
-    <Link key="travel" to="travel" className="list-group-item list-group-item-action bg-info text-white rounded-0">Travel</Link>,
-    <Link key="table" to="table" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Table</Link>,
-    <Link key="vote" to="vote" className="list-group-item list-group-item-action bg-info text-white rounded-0">Vote</Link>,
-    <Link key="drink" to="drink" className="list-group-item list-group-item-action bg-secondary text-white rounded-0">Drink Search</Link>,
+    <NavLink key="home" to="/" end className="list-group-item list-group-item-action rounded-0">Home</NavLink>,
+    <NavLink key="login" to="login" className="list-group-item list-group-item-action rounded-0">Login</NavLink>,
+    <NavLink key="profile" to="profile" className="list-group-item list-group-item-action rounded-0">Profile</NavLink>,
+    <NavLink key="travel" to="travel" className="list-group-item list-group-item-action rounded-0">Travel</NavLink>,
+    <NavLink key="table" to="table" className="list-group-item list-group-item-action rounded-0">Table</NavLink>,
+    <NavLink key="vote" to="vote" className="list-group-item list-group-item-action rounded-0">Vote</NavLink>,
+    <NavLink key="drink" to="drink" className="list-group-item list-group-item-action rounded-0">Drink Search</NavLink>,
 ]
--- src/components/NavBar/SideNav.jsx
+++ src/components/NavBar/SideNav.jsx
@@ -5,13 +5,13 @@ import { NavLinks } from './NavBarUtils';
 function SideNav() {
   return (
     <nav className='collapse d-lg-block sidebar collapse bg-white' id="side-nav">
-        <div className="position-sticky" >
-            <div className="list-group list-group-flush">
-              {NavLinks}
-            </div>
+      <div className="sidebar-sticky">
+        <div className="list-group list-group-flush">
+          {NavLinks}
         </div>
+      </div>
     </nav>
-    
+
   )
 }
 

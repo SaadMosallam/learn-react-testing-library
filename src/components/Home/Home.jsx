6ad65199bb9a46e10d70ade85e332de064f59421
Smosallam <Saad.MOSALLAM@cegedim.com>
2023-01-30 00:30:21 +0200

    apply linter style

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <>
-            <div className='d-flex flex-column mt-5 align-items-center'>
-                <h1 className=''>Learn React Testing Library!</h1>
-                <p>Learn how to test React components using React Testing Library.</p>
-            </div>
-        </>
-
-    );
-}
+  return (
+    <>
+      <div className="d-flex flex-column mt-5 align-items-center">
+        <h1>Learn React Testing Library!</h1>
+        <p>Learn how to test React components using React Testing Library.</p>
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,11 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import React from 'react';
-import Home from './Home';
+import React from "react";
+import Home from "./Home";
 
-describe('Home page tests', () => {
-    test('Home page renders correctly', () => {
-        render(<Home />);
-        expect(screen.getByText(/Learn React Testing Library!/i)).toBeInTheDocument();
-        expect(screen.getByText(/Learn how to test React components using React Testing Library./i)).toBeInTheDocument();
+describe("Home page tests", () => {
+  test("Home page renders correctly", () => {
+    render(<Home />);
+    const heading = screen.getByRole("heading", {
+      name: /Learn React Testing Library!/i,
     });
+    expect(heading).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /Learn how to test React components using React Testing Library./i
+      )
+    ).toBeInTheDocument();
+  });
 });
